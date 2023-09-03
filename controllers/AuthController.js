c71195df65f11fd8ecced3e764634a2031f9b2f0
allenkays <allenkays@tutanota.com>
2023-09-03 19:02:23 +0300

    Fix imports errors

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,31 +7,30 @@ const uuid = require('uuid').v4;
 class AuthController {
   static async getConnect(req, res) {
     const authHeader = req.headers.authorization;
-
+    
     if (!authHeader || !authHeader.startsWith('Basic ')) {
-      return res.status(400).json({ error: 'unauthorized' });
+      return res.status(401).json({ error: 'Unauthorized' });
     }
-
-    /* base64 extraction from email and password */
+    
+    // Extract the base64-encoded email and password
     const base64Credentials = authHeader.split(' ')[1];
     const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
     const [email, password] = credentials.split(':');
 
-    /* Find user */
-    const hashedPassword = sha1(password);
+    // Find the user by email and hashed password
+    const hashedPassword = sha1(password); // Hash the provided password
     const user = await DBClient.db.collection('users').findOne({ email, password: hashedPassword });
 
     if (!user) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
-    /* token generation */
+
+    // Generate a random token
     const token = uuid();
 
-    console.log('Generated Token:', token);
-    /* Store the user ID in Redis with the token as the key for 24 hours */
-    await RedisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);/* 24 hours in seconds */
+    // Store the user ID in Redis with the token as the key for 24 hours
+    await RedisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60); // 24 hours in seconds
 
-    console.log({ token })
     return res.status(200).json({ token });
   }
 
@@ -39,19 +38,19 @@ class AuthController {
     const token = req.header('X-Token');
 
     if (!token) {
-      return res.status(401).json({ error: 'Unauthorized'});
+      return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    /* Retrieve user Id from Redis in Cache */
-    const userId = await RedisClient.get(`auth_${token}`);
+    // Retrieve the user ID from Redis
+    const userId = await redisClient.get(`auth_${token}`);
 
     if (!userId) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    /* Token deletion from redis */
-
+    // Delete the token in Redis
     await RedisClient.del(`auth_${token}`);
+
     return res.status(204).send();
   }
 }
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,6 @@
 import DBClient from '../utils/db';
+import RedisClient from '../utils/redis';
+const { ObjectId } = require('mongodb');
 const sha1 = require('sha1');
 
 class UsersController {
@@ -40,6 +42,30 @@ class UsersController {
       console.error('Error creating user:', error);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
+  }
+
+  static async getMe(req, res) {
+    const token = req.header('X-Token');
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    console.log('Received Token:', token);
+    /* Retrieve the user ID from Redis */
+    const userId = await RedisClient.get(`auth_${token}`);
+    console.log('Retrieved User ID from Redis:', userId);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await DBClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ email: user.email, id: user._id.toString() });
   }
 }
 
